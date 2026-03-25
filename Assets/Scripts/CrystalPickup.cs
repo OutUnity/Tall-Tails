58b0f1200d1aca753acb400601622a3a6e358f51
OutUnity <o2460007@katyisd.org>
2026-03-25 14:40:33 -0500

    audio

--- Assets/Scripts/CrystalPickup.cs
+++ Assets/Scripts/CrystalPickup.cs
@@ -1,21 +1,21 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CrystalPickup : MonoBehaviour
 {
     private int regionID;
+
+    public ParticleSystem pickupEffect;
+    public AudioClip pickupSound;
+    [Range(0f, 2f)] public float volume = 0.3f;
+
     private void Start()
     {
-        // Auto-detect region by parent
         RegionTrigger parentRegion = GetComponentInParent<RegionTrigger>();
+
         if (parentRegion != null)
-        {
             regionID = parentRegion.regionID;
-        }
         else
-        {
-            Debug.LogWarning("CrystalPickup could not find parent RegionTrigger!");
-            regionID = 1; // fallback
-        }
+            regionID = 1;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -23,6 +23,19 @@ public class CrystalPickup : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             CrystalManager.Instance.AddCrystal(regionID);
+
+            // 🔊 Sound
+            if (pickupSound != null)
+                AudioSource.PlayClipAtPoint(pickupSound, transform.position, volume);
+
+            // ✨ Particles
+            if (pickupEffect != null)
+            {
+                pickupEffect.transform.parent = null;
+                pickupEffect.Play();
+                Destroy(pickupEffect.gameObject, 2f);
+            }
+
             Destroy(gameObject);
         }
     }
